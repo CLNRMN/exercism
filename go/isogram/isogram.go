bd553b40422244555fd39aa985aadbe499fabcc9
CLNRMN <trustmeiamcolin@gmail.com>
2020-07-09 17:48:34 +0200

    first version of isogram.go

---
+++ go/isogram/isogram.go
@@ -0,0 +1,28 @@
+package isogram
+
+import (
+	"strings"
+	"unicode"
+)
+
+// IsIsogram returns true or false
+func IsIsogram(iso string) bool {
+	// Set variable iso to lowercase
+	iso = strings.ToLower(iso)
+	// Set default to "false"
+	var check bool
+	// Empty strings are also isograms
+	if iso == "" {
+		check = true
+	}
+	for _, c := range iso {
+		// Check if the character IsLetter and if the count is bigger than 1
+		if strings.Count(iso, string(c)) > 1 && unicode.IsLetter(c) {
+			check = false
+			break
+		} else {
+			check = true
+		}
+	}
+	return check
+}
