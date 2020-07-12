58ac6a28e009a55a3916170cab0b13cc49e0d1c3
CLNRMN <trustmeiamcolin@gmail.com>
2020-07-12 11:26:55 +0200

    Complete Gigasecond

---
+++ go/gigasecond/gigasecond.go
@@ -0,0 +1,10 @@
+// Package gigasecond , random comment
+package gigasecond
+
+// import path for the time package from the standard library
+import "time"
+
+// AddGigasecond should have a comment documenting it. Yes, it should.
+func AddGigasecond(t time.Time) time.Time {
+	return t.Add(time.Second * 1000000000)
+}
