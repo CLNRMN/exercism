d5b243f16b10116902165f31abd46ae8e58b75b9
CLNRMN <trustmeiamcolin@gmail.com>
2020-07-13 13:03:01 +0200

    enhance isogram

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -7,25 +7,19 @@ import (
 
 // IsIsogram returns true or false
 func IsIsogram(iso string) bool {
-	// Set default to "false"
-	var check bool
 	// Initialize Map
 	runeChecker := map[rune]bool{}
-	// Empty strings are also isograms
-	if iso == "" {
-		check = true
-	}
 	for _, c := range iso {
+		// Ignore all special characters
+		if !unicode.IsLetter(c) {
+			continue
+		}
 		// Set character c to lowercase
 		c = unicode.ToLower(c)
-		// Check if the character IsLetter and if the count is bigger than 1
-		if unicode.IsLetter(c) && runeChecker[c] {
-			check = false
-			break
-		} else {
-			runeChecker[c] = true
-			check = true
+		if runeChecker[c] {
+			return false
 		}
+		runeChecker[c] = true
 	}
-	return check
+	return true
 }
